ffd2196ca9072e482958a2e468a02d0658ed357f
Sergei Belokon <srj.belokon@gmail.com>
2022-09-09 15:03:32 +0300

    test: workflow v5

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,12 @@ use chrono;
 use std::{fs, path::Path};
 
 const SITE_ENTRY_POINT: &str = "index.html";
-const WORKFLOW_TEST_VERSION_NUM: u16 = 4;
+const WORKFLOW_TEST_VERSION_NUM: u16 = 5;
 
 fn main() -> std::io::Result<()> {
     let title = format!("workflow test v{}", WORKFLOW_TEST_VERSION_NUM);
     let body = format!("updated: {}", chrono::offset::Utc::now());
-    let index = format!("{}\n{}\n", title, body);
+    let index = format!("{}\r\n{}\r\n", title, body);
     if !Path::new(SITE_ENTRY_POINT).exists() {
         fs::write(SITE_ENTRY_POINT, index)?;
     }

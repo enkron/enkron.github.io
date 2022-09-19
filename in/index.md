009558206d44d0c2aece8dd1773d51fa9fa30f5c
Sergei Belokon <srj.belokon@gmail.com>
2022-09-19 16:24:26 +0300

    fix: cv link

--- in/index.md
+++ in/index.md
@@ -1,1 +1,2 @@
-### hello
+# main
+[cv](/pub/cv.html)
--- src/rend.rs
+++ src/rend.rs
@@ -16,7 +16,7 @@ impl Layout {
             <link rel="stylesheet" type="text/css" href="/css/main.css" />
             <link rel="stylesheet" href="/css/styles.css">
             <link rel="stylesheet" href="/web/hack.css">
-            <title>{}</title>
+            <title>diy site. from {}</title>
             </head>"#,
             github_ref_name
         )

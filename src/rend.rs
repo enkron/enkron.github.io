69fcf6fb2d765595483ea5601d65a4dfa7deda7d
Sergei Belokon <srj.belokon@gmail.com>
2022-09-20 17:05:33 +0300

    test: css styles naming

--- src/rend.rs
+++ src/rend.rs
@@ -14,8 +14,8 @@ impl Layout {
 <head>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" type="text/css" href="/css/main.css" />
-<link rel="stylesheet" href="/css/styles.css">
+<link rel="stylesheet" href="/css/main.css" type="text/css">
+<link rel="stylesheet" href="/css/style.css">
 <link rel="stylesheet" href="/web/hack.css">
 <title>diy site. from {}</title>
 <nav>

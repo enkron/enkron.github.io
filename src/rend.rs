90ed8bf802841b8037fffc3099eaecaef0dc3a39
Sergei Belokon <srj.belokon@gmail.com>
2022-10-07 13:28:23 +0300

    chore: test another favicon variants v4

--- src/rend.rs
+++ src/rend.rs
@@ -18,10 +18,7 @@ impl Layout {
 <link rel="stylesheet" href="/css/style.css">
 <link rel="stylesheet" href="/web/hack.css">
 <title>enkron's junkyard (built from {} branch)</title>
-<link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png">
-<link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png">
-<link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png">
-<link rel="manifest" href="/favicon/site.webmanifest">
+<link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
 <nav>
 <a href="/">home</a>
 </nav>

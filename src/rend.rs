95465798d41d8f67c00ff6c2c1f72582e79326a3
Sergei Belokon <srj.belokon@gmail.com>
2022-10-07 12:42:08 +0300

    feat: add favicons

--- src/rend.rs
+++ src/rend.rs
@@ -17,7 +17,11 @@ impl Layout {
 <link rel="stylesheet" href="/css/main.css" type="text/css">
 <link rel="stylesheet" href="/css/style.css">
 <link rel="stylesheet" href="/web/hack.css">
-<title>diy site. from {}</title>
+<title>enkron's junkyard (built from {} branch)</title>
+<link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png">
+<link rel="manifest" href="/favicon/site.webmanifest">
 <nav>
 <a href="/">home</a>
 </nav>

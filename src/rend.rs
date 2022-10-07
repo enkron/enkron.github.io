10d8941cce518d29cac18a1e93357c6a16ab63f8
Sergei Belokon <srj.belokon@gmail.com>
2022-10-07 13:22:00 +0300

    chore: test another favicon variants v3

--- src/rend.rs
+++ src/rend.rs
@@ -22,7 +22,6 @@ impl Layout {
 <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png">
 <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png">
 <link rel="manifest" href="/favicon/site.webmanifest">
-<link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
 <nav>
 <a href="/">home</a>
 </nav>

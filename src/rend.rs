90ed9b534d0eaf7726e595da94e3aafb12fca681
Sergei Belokon <srj.belokon@gmail.com>
2022-10-11 18:38:18 +0300

    chore: experiments with padding and color of the header

--- src/rend.rs
+++ src/rend.rs
@@ -27,15 +27,17 @@ impl Layout {
 <meta name="msapplication-TileColor" content="\#da532c">
 <meta name="msapplication-config" content="/favicon/browserconfig.xml">
 <meta name="theme-color" content="\#ffffff">
-<nav class="navbar">
-<div class="logo">tree</div>
-<ul class="nav-links">
-<input type="checkbox" id="checkbox_toggle" />
-<label for="checkbox_toggle" class="hamburger">&#9776;</label>
-<div class="menu">
+<nav role="navigation" class="navigation">
+<img class="logo" src="/favicon/favicon-32x32.png" alt="-__-"/>
+<ul>
 <li><a href="/">home</a></li>
 <li><a href="/pub/cv.html">cv</a></li>
-</div>
+<li><a href="/pub/future.html">future</a>
+<ul class="dropdown">
+<li><a href="/pub/subfuture1.html">subfuture1</a></li>
+<li><a href="/pub/subfuture2.html">subfuture2</a></li>
+</ul>
+</li>
 </ul>
 </nav>
 </head>"#,
